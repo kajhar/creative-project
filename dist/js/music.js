14878e7a4be31a10015ab9375ec8942d523c136e
Kaj Harvey <55725972+kajhar@users.noreply.github.com>
2022-04-26 17:04:45 -0600

    synth implementation with list of notes

--- dist/js/music.js
+++ dist/js/music.js
@@ -1,14 +1,14 @@
 //create a synth and connect it to the main output (your speakers)
+const keyboard = ['C4','C#4','D4','D#4','E4','F4','F#4','G4','G#4','A4','A#4','B4','C5','C#5','D5','D#5','E5','F5','F#5','G5','G#5','A5','A#5','B5','C6'];
 const synth = new Tone.Synth().toDestination();
 
 //play a middle 'C' for the duration of an 8th note
 // synth.triggerAttackRelease('C4', '8n');
 function play() {
-  var x = event.clientX;
+  var x = event.clientX/window.innerWidth;
   var y = event.clientY/window.innerHeight;
-  document.getElementById("ther").innerHTML = x + " " + y;
-  var note;
-  if (y>0.5) note = 'C4';
-  else note = 'C5';
-  synth.triggerAttackRelease(note, '100n');
+  var note = keyboard[Math.floor(y*keyboard.length)];
+  var vol = Math.min(1.2-Math.abs(0.5-x)*2,1);
+  document.getElementById("ther").innerHTML = x + '<br>' + y + '<br>'+ note;
+  synth.triggerAttackRelease(note, '8n','+0', vol);
 }
